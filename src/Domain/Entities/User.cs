032f65598a3d47d32c82c782cac96e141179a415
ibarretom <igorbm.23@gmail.com>
2023-06-21 00:12:11 -0300

    Adição de Entidade Usuário(Teste multiplos push)

---
+++ src/Domain/Entities/User.cs
@@ -0,0 +1,26 @@
+﻿using Domain.ValueObjects;
+
+namespace Domain.Entities;
+
+public class User
+{
+    public Guid Id { get; }
+    public string Name { get; set; }
+    public string Email { get; }
+    public string Phone { get; set; }
+    public string Password { get; set; }
+    public UserRole Role { get; }
+    public DateTime CreatedAt { get; }
+
+    public User(string name, string email, string phone, string password, UserRole role)
+    {
+        Id = Guid.NewGuid();
+        Name = name;
+        Email = email;
+        Phone = phone;
+        Password = password;
+        Role = role;
+        CreatedAt = DateTime.Now;
+    }
+
+}
