19980250b5a58c1374c784331f49a907f016c32a
ibarretom <igorbm.23@gmail.com>
2023-06-25 23:24:09 -0300

    DTO de request de criação de usuário e adição de automapper

---
+++ src/Domain/Services/Mappers/AutoMapperProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Domain.Entities;
+using Domain.ValueObjects.DTO;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("ApplicationTest")]
+[assembly: InternalsVisibleTo("DomainTest")]
+namespace Domain.Services.Mappers;
+
+internal class AutoMapperProfile : Profile
+{
+        public AutoMapperProfile()
+        {
+            CreateMap<UserRequestDTO, User>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore())
+                .ForMember(dest => dest.Roles, opt => opt.Ignore())
+                .ForMember(dest => dest.CreatedAt, opt => opt.Ignore())
+                .ForMember(dest => dest.Password, opt => opt.Ignore());
+        }
+}
---
+++ src/Domain/ValueObjects/DTO/UserRequestDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace Domain.ValueObjects.DTO
+{
+    public class UserRequestDTO
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Password { get; set; }
+
+        public UserRequestDTO(string name, string email, string phone, string password)
+        {
+            Name = name;
+            Email = email;
+            Phone = phone;
+            Password = password;
+        }
+    }
+}
