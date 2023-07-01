2becb022dbb8a1c2dd377f563215ac16529bd802
ibarretom <igorbm.23@gmail.com>
2023-07-01 19:13:25 -0300

    Entidade de barbearia

---
+++ src/Domain/Entities/Barbers/Barber.cs
@@ -0,0 +1,26 @@
+﻿namespace Domain.Entities.Barbers;
+
+internal class Barber
+{
+    public string Name { get; set; }
+    public string Identifier { get; set; }
+    public string Avatar { get; set; }
+    public HashSet<Branch> Branch { get; } = new HashSet<Branch>();
+
+    public Barber(string name, string identifier, string avatar)
+    {
+        Name = name;
+        Identifier = identifier;
+        Avatar = avatar;
+    }
+
+    public void AddBranch(Branch branch)
+    {
+        Branch.Add(branch);
+    }
+
+    public void RemoveBranch(Branch branch)
+    {
+        Branch.RemoveWhere(existentBranch => branch.Identifier == existentBranch.Identifier);
+    }
+}
