485985474051c3c738d7d2f6c6971c6f0fe58c7b
ibarretom <igorbm.23@gmail.com>
2023-07-02 18:41:54 -0300

    DTO de criaçao de barbearia

---
+++ src/Domain/ValueObjects/DTO/Barber/BranchDTO.cs
@@ -0,0 +1,18 @@
+﻿using Domain.ValueObjects.Addresses;
+
+namespace Domain.ValueObjects.DTO.Barber;
+
+internal class BranchDTO
+{
+    public string Identifier { get; set; }
+    public Address Address { get; set; }
+    public string Phone { get; set; }
+    public string Email { get; set; }
+
+    public BranchDTO(string identifier, Address address, string phone, string email) {
+        Identifier = identifier;
+        Address = address;
+        Phone = phone;
+        Email = email;
+    }
+}
---
+++ src/Domain/ValueObjects/DTO/Barber/CreateCompanyRequestDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace Domain.ValueObjects.DTO.Barber;
+
+internal class CreateCompanyRequestDTO
+{
+    public Guid OwnerId { get; set; }
+    public string Identifier { get; set; }
+    public string Name { get; set; }
+    public BranchDTO Branch { get; set; }
+
+    public CreateCompanyRequestDTO(Guid ownerId, string identifier, string name, BranchDTO branch)
+    {
+        OwnerId = ownerId;
+        Identifier = identifier;
+        Name = name;
+        Branch = branch;
+    }
+}
