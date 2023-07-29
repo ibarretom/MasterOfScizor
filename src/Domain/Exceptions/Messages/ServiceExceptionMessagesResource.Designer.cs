69371f4300d52e7f7a584ecb4ba253d24384e976
ibarretom <igorbm.23@gmail.com>
2023-07-29 14:56:45 -0300

    DTO de update de serviço + Resource de mensagem de erro

--- src/Domain/Exceptions/Messages/ServiceExceptionMessagesResource.Designer.cs
+++ src/Domain/Exceptions/Messages/ServiceExceptionMessagesResource.Designer.cs
@@ -22,14 +22,14 @@ namespace Domain.Exceptions.Messages {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class CompanyExceptionMessagesResource {
+    internal class ServiceExceptionMessagesResource {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
         private static global::System.Globalization.CultureInfo resourceCulture;
         
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal CompanyExceptionMessagesResource() {
+        internal ServiceExceptionMessagesResource() {
         }
         
         /// <summary>
@@ -39,7 +39,7 @@ namespace Domain.Exceptions.Messages {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Domain.Exceptions.Messages.CompanyExceptionMessagesResource", typeof(CompanyExceptionMessagesResource).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Domain.Exceptions.Messages.ServiceExceptionMessagesResource", typeof(ServiceExceptionMessagesResource).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
@@ -61,47 +61,11 @@ namespace Domain.Exceptions.Messages {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Branch was not found.
+        ///   Looks up a localized string similar to The category was not found..
         /// </summary>
-        internal static string BRANCH_NOT_FOUND {
+        internal static string CATEGORY_NOT_FOUND {
             get {
-                return ResourceManager.GetString("BRANCH_NOT_FOUND", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Company was not found.
-        /// </summary>
-        internal static string COMAPNY_NOT_FOUND {
-            get {
-                return ResourceManager.GetString("COMAPNY_NOT_FOUND", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Company already exists.
-        /// </summary>
-        internal static string COMPANY_ALREADY_EXISTS {
-            get {
-                return ResourceManager.GetString("COMPANY_ALREADY_EXISTS", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Service already exists.
-        /// </summary>
-        internal static string SERVICE_ALREADY_EXISTS {
-            get {
-                return ResourceManager.GetString("SERVICE_ALREADY_EXISTS", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Service was not found.
-        /// </summary>
-        internal static string SERVICE_NOT_FOUND {
-            get {
-                return ResourceManager.GetString("SERVICE_NOT_FOUND", resourceCulture);
+                return ResourceManager.GetString("CATEGORY_NOT_FOUND", resourceCulture);
             }
         }
     }
---
+++ src/Domain/ValueObjects/DTO/Barber/ServiceUpdateRequestDTO.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities.Barbers.Service;
+
+namespace Domain.ValueObjects.DTO.Barber;
+
+internal class ServiceUpdateRequestDTO : ServiceRequestDTO
+{
+    public Guid Id { get; set; }
+
+    public ServiceUpdateRequestDTO(Guid id, Guid branchId, Category? category, string name, string description, decimal price, decimal promotionalPrice, bool isPromotionActive, bool active, TimeSpan duration) : base(branchId, category, name, description, price, promotionalPrice, isPromotionActive, active, duration)
+    {
+        Id = id;
+    }
+}
